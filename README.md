57a27dc2d947ad09c8a72d42e8d0e1d8fcd6a0e5
mdeiml <matthias@deiml.net>
2016-04-12 18:56:18 +0200

    Identity system working

--- README.md
+++ README.md
@@ -12,3 +12,12 @@ The app should have a database so the game state isn't lost accidentally. The da
 * Order of the politics in the stack
 * president, cancelor, next cancelor
 * game state
+
+## Gameplay
+
+Every turn has 4 to 5 phases:
+* Choosing the chancellor
+* Electing the chancellor
+* The president discards one policy
+* The chancellor discards one policy
+* Extra actions by the president
--- src/main/java/com/mdeiml/secrethitler/ViewRolesActivity.java
+++ src/main/java/com/mdeiml/secrethitler/ViewRolesActivity.java
@@ -6,17 +6,18 @@ import android.widget.*;
 import android.view.*;
 
 public class ViewRolesActivity extends Activity {
-    
+
     private int currentP;
     private boolean open;
-    
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_roles);
-        ((TextView)findViewById(R.id.roleName)).setText("TEST_NAME_DO_NOT_READ"); //TODO
+        final TextView name = (TextView)findViewById(R.id.roleName);
         final ImageView img = (ImageView)findViewById(R.id.roleImg);
         final TextView message = (TextView)findViewById(R.id.roleMessage);
         final Game game = (Game)getIntent().getSerializableExtra("com.mdeiml.secrethitler.Game");
+        name.setText(game.getName(0));
         currentP = 0;
         open = false;
         img.setOnClickListener(new ImageView.OnClickListener() {
@@ -26,18 +27,34 @@ public class ViewRolesActivity extends Activity {
                     if(currentP < game.getNumPlayers()) {
                         img.setImageDrawable(getResources().getDrawable(R.drawable.party_secret));
                         open = false;
-                    }
+                        message.setText("Tap to view identity");
+                        name.setText(game.getName(currentP));
+                    } //TODO: else
                 }else {
                     open = true;
                     switch(game.getRole(currentP)) {
                         case 'l':
                             img.setImageDrawable(getResources().getDrawable(R.drawable.party_liberal));
+                            message.setText("Tap to hide identity");
                             break;
                         case 'f':
                             img.setImageDrawable(getResources().getDrawable(R.drawable.party_fascist));
+                            String fascists = "";
+                            String hitler = "";
+                            for(int i = 0; i < game.getNumPlayers(); i++) {
+                                if(game.getRole(i) == 'f' && i != currentP) {
+                                    fascists += game.getName(i)+", ";
+                                }else if(game.getRole(i) == 'h') {
+                                    hitler = game.getName(i);
+                                }
+                            }
+                            fascists = fascists.substring(0, fascists.length() - 2);
+                            String msg = "The other fascists are: "+fascists+"\n"+hitler+" is Hitler\nTap to hide identity";
+                            message.setText(msg);
                             break;
                         case 'h':
                             img.setImageDrawable(getResources().getDrawable(R.drawable.party_hitler));
+                            message.setText("Tap to hide identity");
                             break;
                     }
                 }
